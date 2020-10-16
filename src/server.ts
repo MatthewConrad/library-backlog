193fc803a9aac77da77bd3fa5c0ad166f7ab1a5d
Matthew Conrad <conrad.matthewj@gmail.com>
2020-10-16 16:45:25 -0400

    Added reload module so browser refreshes when server restarts.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const reload = require("reload");
 
 const app: express.Application = express();
 
 app.use(express.static("build"));
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000.");
-});
+reload(app)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+    .then((reloadReturned: any) => {
+        app.listen(5000, () => {
+            console.log("Server started on port 5000.");
+        });
+    })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    .catch(function (err: any) {
+        console.error(
+            "Reload could not start, could not start server/sample app",
+            err
+        );
+    });
